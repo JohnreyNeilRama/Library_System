2822e2414fb40033f90709d44f3a89a67d798667
Johnrey Neil M. Rama <johnreyneil.rama@gmail.com>
2026-04-21 12:18:08 +0800

    Landing pages with login and Register page design

---
+++ LibrarySystem/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+
+}
